beca027ee903e05aaaa2cdaa7ab1321bf14f3fc8
alex <desorbaix@free.fr>
2020-06-23 15:45:18 +0200

    creation d'un objet pour le crud

--- app/app.js
+++ app/app.js
@@ -1,23 +1,34 @@
-function httpCRUD(method,ressources,callback,id,body){
-    var xhr = new XMLHttpRequest();
-    var url='http://localhost:2500/'+ressources+(id==undefined?'':'/'+id);
-    xhr.open(method, url);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onreadystatechange = function (evt) {
-        if (evt.currentTarget.readyState < XMLHttpRequest.DONE) return;
-        var reponseJS=JSON.parse(evt.currentTarget.response);
-        callback(reponseJS);
+var RestCrud = function (baseUrl) {
+    function httpCRUD(method, ressources, callback, id, body) {
+        var xhr = new XMLHttpRequest();
+        var url = baseUrl + ressources + (id == undefined ? '' : '/' + id);
+        xhr.open(method, url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.onreadystatechange = function (evt) {
+            if (evt.currentTarget.readyState < XMLHttpRequest.DONE) return;
+            var reponseJS = JSON.parse(evt.currentTarget.response);
+            callback(reponseJS);
+        }
+        xhr.send(JSON.stringify(body));
     }
-    xhr.send(JSON.stringify(body));
+
+    function _get(callback, ressource, id) { httpCRUD('GET', ressource, callback, id); }
+ 
+    function _put(callback, ressource, id, body) { httpCRUD('PUT', ressource, callback, id, body); }
+    function _patch(callback, ressource, id, body) { httpCRUD('PATCH', ressource, callback, id, body); }
+    function _delet(callback, ressource, id) { httpCRUD('DELETE', ressource, callback, id); }
+    
+    // exposition des fonctions privées
+    this.get=_get;
+    this.post=function(callback, ressource, body) { httpCRUD('POST', ressource, callback, undefined, body); }
 }
 
-function get(callback,ressource, id) {httpCRUD('GET',ressource,callback,id);}
-function post(callback,ressource,body) {httpCRUD('POST',ressource,callback,undefined,body);}
-function put(callback,ressource, id,body) {httpCRUD('PUT',ressource,callback,id,body);}
-function patch(callback,ressource, id,body) {httpCRUD('PATCH',ressource,callback,id,body);}
-function delet(callback,ressource, id) {httpCRUD('DELETE',ressource,callback,id);}
+var crud=new RestCrud('http://localhost:2500/');
+crud.get(function (reponse) { console.log('traitement d\'un objet', reponse) }, 'messages', 1);
 
-get(function(reponse){console.log('traitement d\'un objet',reponse)},'messages',1);
-get(function(reponse){console.log('traitement d\'une array',reponse)},'messages');
+// get(function (reponse) { console.log('traitement d\'un objet', reponse) }, 'messages', 1);
+// get(function (reponse) { console.log('traitement d\'une array', reponse) }, 'messages');
+// post(function (reponse) { console.log('traitement d\'ajout d\'un message', reponse) }, 'messages', { from: "me", to: 'zoidberg', message: "valeur du message" });
+
 
