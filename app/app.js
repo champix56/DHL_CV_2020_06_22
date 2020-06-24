17ec5036df5ff4e80a8ed41b0c0509baaed79f60
alex <desorbaix@free.fr>
2020-06-24 10:38:49 +0200

    creation d'un div message  par une fonction avec gestion des class

--- app/app.js
+++ app/app.js
@@ -19,6 +19,23 @@ domUser.forEach(function(element){
 // })
 document.querySelector('form').addEventListener('submit',function(evt){
     evt.preventDefault();
-    console.log(evt);
-    console.log(evt.target[0].value,evt.target[1].value)
+    //console.log(evt);
+    // console.log(evt.target[0].value,evt.target[1].value)
+    
+    
+    // mauvaise pratique car suppression des objets du dom dans la balise #tchat-message
+    //et aussi de tous les events associés a cette balise
+    // document.querySelector('#tchat-messages').innerHTML+=evt.target[0].value;
+    addMessageInMessages({message:evt.target[0].value})
+    evt.target.reset();
+   
 })
+function addMessageInMessages(objMessage,mine) {
+     //bonne ratique pour la conservation de la structure html presente dans la balise conteneur
+    var message=document.createElement('div');
+    message.className="message";
+    if(mine===true)message.classList.add('mine');
+    message.innerHTML=objMessage.message;
+    //console.log(message);
+    document.querySelector('#tchat-messages').append(message);
+}
